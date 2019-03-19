9aaad76d7c81f015fb9cde77b229e60b4b16f516
209278 <209278@CARBON.CTI.P.LODZ.PL>
2019-03-19 19:20:23 +0100

    elementNotFound test

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -16,5 +16,28 @@ public class BinarySearchTest {
         assertEquals(true, searchResult.isFound());
         assertEquals( key, seq[searchResult.getPosition()]);
     }
-    
+
+    @Test
+    public void emptySeq() {
+        int[] seq = {};
+        int key = 5;
+
+        try{
+            BinarySearch.search(key, seq);
+        }catch (Exception e){
+            assertEquals(new IllegalArgumentException(), e);
+        }
+    }
+
+    @Test
+    public void elementNotFound() {
+        int[] seq = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 11;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+
+        assertEquals(false, searchResult.isFound());
+        assertEquals( -1, searchResult.getPosition());
+    }
+
 }
