00e3534be9f90dafb1a3328d728c7a56a8d60a4b
209278 <209278@edu.p.lodz.pl>
2019-03-24 19:53:26 +0100

    test_v1.4

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -46,6 +46,16 @@ public class BinarySearchTest {
         elementFound(seq, key);
     }
 
+    @Test
+    public void manyElementsAndKeyAtLastPosition(){
+        int[] seq = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 10;
+
+        elementFound(seq, key);
+    }
+
+
+
     public void elementFound(int[] SEQ, int KEY) {
         int[] seq = SEQ;
         int key = KEY;
@@ -57,8 +67,8 @@ public class BinarySearchTest {
     }
 
     public void elementNotFound(int[] SEQ, int KEY) {
-        int[] seq = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int key = 11;
+        int[] seq = SEQ;
+        int key = KEY;
 
         SearchResult searchResult = BinarySearch.search(key, seq);
 
